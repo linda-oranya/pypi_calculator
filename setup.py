aab9f4c266be9f68132d93b129bd78091d121c0c
linda oranya <lindaoranya@lindas-MacBook-Pro.local>
2021-07-03 09:34:15 +0300

    updated test code, gitignore

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ecalculator-pkg-linda-oranya",
-    version="0.0.1",
+    version="0.0.2",
     author="Linda Oranya",
     author_email="oranyalinda7@gmail.com",
     description="A calculator written in python",
